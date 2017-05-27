c55db39e3a060c87efa82b748263947fbea0760a
vls48 <vls48@drexel.edu>
2017-05-26 20:16:19 -0400

    update
    worked on mobile, change bg/font/colors/ added nav

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 //document.getElementById("button").onclick=newPage(){
 //	window.location.href = "http://vlstewart.com/idm240/final/content.html"
 //};
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
