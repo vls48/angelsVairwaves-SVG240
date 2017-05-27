a97da2220f91c320ca89bff416d868810c65b08e
vls48 <vls48@drexel.edu>
2017-05-27 12:00:12 -0400

    additions
    added fade in, sound, link styling, fixed display on mobile

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 //	window.location.href = "http://vlstewart.com/idm240/final/content.html"
 //};
 
+
+var audioPlayer = document.getElementById('audioplayer');
+var isPlaying = false;
+
 /* Open */
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
@@ -10,4 +14,27 @@ function openNav() {
 /* Close */
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
+}
+
+
+function fade2Content() {
+  console.log('fade2Content called');
+  document.body.classList.remove('blackScreen');
+}
+
+
+function audioToggle(msg) {
+
+    if (isPlaying) {
+        audioPlayer.pause()
+        isPlaying = false
+        document.getElementById('soundicon').classList.add('hide');
+        
+    } else {
+        audioPlayer.play()
+        isPlaying = true
+        document.getElementById('soundicon').classList.remove('hide');
+    }
+
+    //audioPlayer.pause()
 }
